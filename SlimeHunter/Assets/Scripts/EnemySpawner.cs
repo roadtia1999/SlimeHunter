9c037a3b9c856da9870520d827c240d28d4aea7e
roadtia1999 <132754149+roadtia1999@users.noreply.github.com>
2024-05-03 15:29:22 +0900

    level 2 and 3

--- SlimeHunter/Assets/Scripts/EnemySpawner.cs
+++ SlimeHunter/Assets/Scripts/EnemySpawner.cs
@@ -7,6 +7,7 @@ public class EnemySpawner : MonoBehaviour
     public List<GameObject> enemySlimes;
     private float totalTime;
     private float spawnTime;
+    public static bool endTime;
 
     public float MaxX;
     public float MinX;
@@ -15,36 +16,33 @@ public class EnemySpawner : MonoBehaviour
 
     public int waveTime;
     public float Lvl1_SpawnTime;
+    public float Lvl2_SpawnTime;
+    public float Lvl3_SpawnTime;
+    public float Lvl4_SpawnTime;
+    public float Lvl5_SpawnTime;
+    public float Lvl6_SpawnTime;
+    public float Lvl7_SpawnTime;
+    public float Lvl8_SpawnTime;
+    public float Lvl9_SpawnTime;
+    public float Lvl10_SpawnTime;
+
+    private bool Lvl3_Pattern_On;
 
     // Start is called before the first frame update
     void Start()
     {
         totalTime = 0;
         spawnTime = 0;
+        endTime = false;
+
+        Lvl3_Pattern_On = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        int rand = Random.Range(1, 4);
-        Vector3 spawn = new Vector3();
+        Vector3 spawn = RandomSpawnPoint();
         Quaternion qtn = Quaternion.identity;
-
-        switch (rand)
-        {
-            case 1:
-                spawn = new Vector3(MinX, Random.Range(MinY, MaxY), 0);
-                break;
-            case 2:
-                spawn = new Vector3(MaxX, Random.Range(MinY, MaxY), 0);
-                break;
-            case 3:
-                spawn = new Vector3(Random.Range(MinX, MaxX), MinY, 0);
-                break;
-            case 4:
-                spawn = new Vector3(Random.Range(MinX, MaxX), MaxY, 0);
-                break;
-        }
 
         totalTime += Time.deltaTime;
         spawnTime += Time.deltaTime;
@@ -58,8 +56,79 @@ public class EnemySpawner : MonoBehaviour
                     Instantiate(enemySlimes[0], spawn, qtn);
                 }
                 break;
+            case 1:
+                if (spawnTime > Lvl2_SpawnTime)
+                {
+                    spawnTime = 0;
+                    Instantiate(enemySlimes[0], spawn, qtn);
+                }
+                break;
+            case 2:
+                if (Lvl3_Pattern_On)
+                {
+                    if (spawnTime > Lvl3_SpawnTime)
+                    {
+                        spawnTime = 0;
+                        Instantiate(enemySlimes[0], spawn, qtn);
+                    }
+                }
+                else
+                {
+                    StartCoroutine(Lvl3Pattern());
+                    Lvl3_Pattern_On = true;
+                }
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
             default:
                 break;
         }
+    }
+
+    Vector3 RandomSpawnPoint()
+    {
+        int rand = Random.Range(1, 4);
+
+        switch (rand)
+        {
+            case 1:
+                return new Vector3(MinX, Random.Range(MinY, MaxY), 0);
+                break;
+            case 2:
+                return new Vector3(MaxX, Random.Range(MinY, MaxY), 0);
+                break;
+            case 3:
+                return new Vector3(Random.Range(MinX, MaxX), MinY, 0);
+                break;
+            case 4:
+                return new Vector3(Random.Range(MinX, MaxX), MaxY, 0);
+                break;
+            default:
+                return new Vector3(MinX, Random.Range(MinY, MaxY), 0);
+                break;
+        }
+    }
+
+    IEnumerator Lvl3Pattern()
+    {
+        do
+        {
+            Instantiate(enemySlimes[1], RandomSpawnPoint(), Quaternion.identity);
+            yield return new WaitForSeconds(9.5f);
+        } while (totalTime < 180);
     }
 }
